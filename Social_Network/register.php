9b1216be6660c147c24cb20f28a7106b50758c46
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-15 16:58:37 +0200

    VERIF
    cfezGN

---
+++ Social_Network/register.php
@@ -0,0 +1,41 @@
+
+<?php
+
+    //Si le formulaire a ete soumis
+    if($_POST['register'])) {
+      //si tous les champs demandes ont ete remplis
+      if(!empty($_POST['name']) && (!empty($_POST['pseudo']) && (!empty($_POST['email']) && (!empty($_POST['password'])
+      && !empty($_POST['password_confirm]))'])) {
+
+        $errors = []; //Tableau contenant l'ens des erreurs
+        exctract($_POST);
+
+        if(mb_strlen($pseudo) < 3) {
+          $errors[] = "Taille minimale du pseudo = 3 caracteres"
+        }
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          $errors[] = "Adresse email invalide";
+        }
+
+        if(mb_strlen($password) < 6) {
+          $errors[] = "Taille minimale du mot de passe = 6 caracteres"
+        }
+        else {
+          if($password == $password_confirm){
+            $errors[] = "Les mots de passe ne concordent pas"
+          }
+        }
+
+        if(is_already_in_use(''))
+
+      //si tous les champs demandes n'ont pas ete remplis
+      } else {
+        $errors[] = "Remplissez tous les champs";
+      }
+    }
+
+
+  ?>
+
+  <?php require('views/register.view.php'); ?>
