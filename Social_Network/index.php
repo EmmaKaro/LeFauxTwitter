12aae8163317bf9e3d3540df8e755edaa90c5377
karolane <Karolane.PICOT@etu.uca.fr>
2020-06-15 14:59:23 +0200

    début page d'acceuille

---
+++ Social_Network/index.php
@@ -0,0 +1,5 @@
+
+<?php
+
+// Recherche du dossier index.views
+require_once __DIR__ . '/views/index.views.php';
---
+++ Social_Network/views/index.views.php
@@ -0,0 +1,73 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+ <head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="description" content="Petit Twitter de petites developpeuses">
+   <meta name="author" content="Emma Provot, Karolane Picot">
+
+   <title>Le Faux Twitter</title>
+
+   <!-- Bootstrap core CSS -->
+   <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" href="assets/css/main.css"/>
+
+   <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/4.5/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/4.5/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/4.5/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon.ico">
+<meta name="msapplication-config" content="/docs/4.5/assets/img/favicons/browserconfig.xml">
+<meta name="theme-color" content="#563d7c">
+
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+ </head>
+
+ <body>
+
+   <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+     <div class="container">
+       <div class="navbar-header">
+         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="sr-only">Toggle navigation</span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+         </button>
+         <a class="navbar-brand" href="#">Le Faux Twitter</a>
+       </div>
+       <div class="collapse navbar-collapse">
+         <ul class="nav navbar-nav">
+           <li class="active"><a href="#">Acceuil</a></li>
+           <li><a href="#about">Connexion</a></li>
+           <li><a href="#contact">Inscription</a></li>
+         </ul>
+       </div>
+     </div>
+   </div>
+
+<dic class="container">
+ <div class="jumbotron">
+   <h1>Le Faux Twitter</h1>
+   <p>
+     Le Faux Twitter est une sorte de Twitter version Leader Price. <br/>
+     Ceci est un projet de PHP dans le cadre de l'IUT d'informatique graphique du Puy-en-Velay. </br>
+     Mais vous pouvez tout de meme l'utilisez comme un mini-Twitter ! </br>
+     Alors n'hesitez pas et <a href="inscription.php">rejoignez-nous </a> !
+   </p>
+   <a href="inscription.php" class="btn btn-primary btn-lg">Je m'inscris</a>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
